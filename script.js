5240b37ae445fb6a6fdd0506f15da1bc6afb2626
solexy79 <64537004+solexy79@users.noreply.github.com>
2020-08-13 14:31:54 +0100

    init commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//ALarm clock controller 
+var alarmClockController = (function(){
+  //some code
+})();
+
+// ******************
+//UI controller 
+var UIController = (function(){
+
+  //some other code
+})();
+
+
+// **************
+// GLOBAL APP CONTROLLER
+var Controller = (function(){
+
+  //some other code
+})();
